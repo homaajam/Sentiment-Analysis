0661a8a3c3b72087fb5819cc54aec1f1e980eee5
Homa <hmajam83@gmail.com>
2026-07-05 09:49:26 +0330

    fix typo

--- source/vocab.py
+++ source/vocab.py
@@ -2,8 +2,8 @@ from collections import Counter
 
 class Vocab:
   def __init__(self, min_freq=2):
-    self.word2indx={"<pad>":0 , "<unk>":1}
-    self.indx2word={0:"<pad" , 1:"<unk>"}
+    self.word2idx={"<pad>":0 , "<unk>":1}
+    self.idx2word={0:"<pad>" , 1:"<unk>"}
     self.min_freq= min_freq
 
   def build_vocab(self, sentences):
@@ -13,23 +13,14 @@ class Vocab:
     index= 2
     for word, freq in counter.items():
       if freq >= self.min_freq:
-        self.word2indx[word]= index
-        self.indx2word[index]= word
+        self.word2idx[word]= index
+        self.idx2word[index]= word
         index+=1
 
-  def encode(self, sentance):
+  def encode(self, sentence):
     encoded=[]
-    for word in sentance:
+    for word in sentence:
       encoded.append(self.word2indx.get(word, 1))
     return encoded
-  
-test=[
-  ["i", "love", "it"],
-  ["it", "was", "amazing"]
-]
-vocab= Vocab() 
-vocab.build_vocab(test)
-print(vocab.encode(["i", "love", "it"]))
-print(vocab.encode(["it", "was", "amazing"]))
-    
+
     
