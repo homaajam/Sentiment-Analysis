44eb972eca0e95934fc217658963352c11546230
Homa <hmajam83@gmail.com>
2026-07-05 08:46:53 +0330

    add vocab class for text encoding

---
+++ source/vocab.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+class Vocab:
+  def __init__(self, min_freq=2):
+    self.word2indx={"<pad>":0 , "<unk>":1}
+    self.indx2word={0:"<pad" , 1:"<unk>"}
+    self.min_freq= min_freq
+
+  def build_vocab(self, sentences):
+    counter= Counter()
+    for sentence in sentences:
+      counter.update(sentence)
+    index= 2
+    for word, freq in counter.items():
+      if freq >= self.min_freq:
+        self.word2indx[word]= index
+        self.indx2word[index]= word
+        index+=1
+
+  def encode(self, sentance):
+    encoded=[]
+    for word in sentance:
+      encoded.append(self.word2indx.get(word, 1))
+    return encoded
+  
+test=[
+  ["i", "love", "it"],
+  ["it", "was", "amazing"]
+]
+
+    
+    
