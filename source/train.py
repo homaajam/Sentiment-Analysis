5c6f8d4fc82561f2a27c89e2cd3d1d21b7813e2b
Homa <hmajam83@gmail.com>
2026-07-16 10:36:23 +0330

    Add training loop with loss and optimizer

---
+++ source/train.py
@@ -0,0 +1,20 @@
+import torch
+
+def train(model, dataloader, optimizer, criterion, device):
+  model.train()
+  total_loss= 0
+  
+  for x, y in dataloader:
+    x, y= x.to(device), y.to(device).float()
+
+    optimizer.zero_grad()
+
+    output= model(x).squeeze
+
+    loss= criterion(output, y)
+    loss.backward()
+    optimizer.step()
+    total_loss += loss.item()
+
+    return total_loss/len(dataloader)
+
