4ee2e63b8fa03323b1f11a9363c945a3b540c12a
Homa <hmajam83@gmail.com>
2026-07-18 12:04:05 +0330

    load  and preprocess data

--- data/main.py
+++ data/main.py
@@ -1,3 +1,10 @@
 import pandas as pd
+from source.preprocess import clean_text, tokenize
+
 
 df= pd.read_csv("data/imdb.csv")
+texts= [tokenize(clean_text(t)) for t in df["review"]]
+labels= df["sentiment"].map({"positive":1, "negative":0}).tolist()
+
+print(texts[0])
+print(labels[0])
